54cfa7012fc633ecd485429e905000f590c5b81a
jaeseopkim <jaeseopkim@smilegate.com>
2022-01-09 15:23:54 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import {Button} from './Button';
 import styles from './App.module.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [search, setSearch] = useState("");
+    const inclease = () => setCounter((prev) => prev + 1);
+    const changeValue = (e) => setSearch(() => e.target.value);
+
+    useEffect(() => console.log('is only first'), []);
+    useEffect(()=> {
+        console.log('updated Search :: ', search);
+    }, [search]);
+
+    useEffect(()=> {
+        console.log('updated counter :: ', counter);
+    }, [counter]);
+
     return (
         <div className={styles.title}>
-            <h1>Welcome back!!!</h1>
-            <Button text="Continue"/>
+            <input
+                type="text"
+                value={search}
+                onChange={changeValue}
+                placeholder="search for here"
+            >
+            </input>
+            <h1>{counter}</h1>
+            <button onClick={inclease}>inclease</button>
         </div>
     );
 }
